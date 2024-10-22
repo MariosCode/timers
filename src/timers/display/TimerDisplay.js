9981a5b0fe71364e0cc1cd51a3d442db979aad4e
MariosCode <mariocorteguera@gmail.com>
2024-10-21 20:17:27 -0400

    small comment changes

--- src/timers/display/TimerDisplay.js
+++ src/timers/display/TimerDisplay.js
@@ -1,7 +1,7 @@
 
 /**  
  * @class TimerDisplay
- * @classdesc The superclass for all TimerDisplay subclasses.
+ * @classdesc The superclass for all TimerDisplay subclasses. This class exists solely for type checking purposes.
  */
 export class TimerDisplay{
     constructor(){
--- src/timers/timer/Timer.js
+++ src/timers/timer/Timer.js
@@ -1,6 +1,6 @@
 /**  
  * @class Timer
- * @classdesc The superclass for all Timer subclasses.
+ * @classdesc The superclass for all Timer subclasses. This class exists solely for type checking purposes.
  */
 export class Timer{
     constructor(){
